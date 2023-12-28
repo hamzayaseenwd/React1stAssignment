696cb840bd123544b3b1271e315bd172ca98ee54
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-12-28 23:38:25 +0500

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header'
+import Hero from './Components/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+      
+      <div>
+      <Header/>
+      {/* <Hero/> */}
+      </div>
+      
+    </>
+  )
+}
+
+export default App
