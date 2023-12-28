f51055991ec14d97d5db90323e4aae4363d6cdc2
hamzayaseenwd <hamzayaseen836@gmail.com>
2023-12-29 00:01:39 +0500

    Update

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='flex justify-around'>
+      <div>Logo</div>
+      <div><button type="submit">Sign Up</button></div>
+    </div>
   )
 }
 
